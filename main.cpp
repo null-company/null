072bc65b91f86b7ec8810295883d8c66defe7be0
Roman <b111forjob@gmail.com>
2021-10-14 19:29:04 +0700

    Add example project

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML\Graphics.hpp>
+
+int main() {
+    sf::RenderWindow sfmlWin(sf::VideoMode(1280, 720), "{[Null]}");
+
+    sf::Texture nullTexture;
+    if (!nullTexture.loadFromFile("../null.jpg"))
+    {
+      return EXIT_FAILURE;
+    }
+    sf::Sprite nullPicture(nullTexture);
+
+    while (sfmlWin.isOpen()) {
+
+        sf::Event e;
+        while (sfmlWin.pollEvent(e)) {
+
+            switch (e.type) {
+            case sf::Event::EventType::Closed:
+                sfmlWin.close();
+                break;
+            }
+        }
+
+        sfmlWin.clear();
+        sfmlWin.draw(nullPicture);
+        sfmlWin.display();
+    }
+    return 0;
+}
