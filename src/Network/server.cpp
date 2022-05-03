44fc898d7c35e4fd17094916057db8aed2a45d14
Roman <42515597+ghB111@users.noreply.github.com>
2022-05-03 15:59:38 +0700

    wip sync
    This is meant to be squashed later

--- src/Network/server.cpp
+++ src/Network/server.cpp
@@ -2,7 +2,8 @@
 #include <plog/Appenders/ConsoleAppender.h>
 #include <plog/Init.h>
 #include <plog/Formatters/TxtFormatter.h>
-#include <client/Client.h>
+#include <SceneLoader.hpp>
+#include <MainLoop.hpp>
 
 int main() {
     static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
@@ -11,11 +12,37 @@ int main() {
     serverArbiter.listen("127.0.0.1", 5000);
     serverArbiter.launch();
 
-    while (true) {
-        std::string oper;
-        std::cin >> oper;
-        if (oper == "exit") {
-            break;
+//    while (true) {
+//        std::string oper;
+//        std::cin >> oper;
+//        if (oper == "exit") {
+//            break;
+//        }
+//    }
+    std::string levelToLoad; // see /menu or /network-demo or /demo
+    bool sceneIsLoaded = false;
+    std::cout << "Type level uri to load" << std::endl;
+    while(!sceneIsLoaded) {
+        std::cin >> levelToLoad;
+        try {
+            null::SceneLoader::loadSceneFromFile(levelToLoad);
+            sceneIsLoaded = true;
+        } catch (const null::UnknownSceneException& e) {
+            std::cout << "Unknown scene, try again" << std::endl;
+        }
+    }
+
+    if (null::MainLoop::isServer) {
+        null::MainLoop::serverArbiter = new ServerArbiter(null::MainLoop::run);
+        auto& serverArbiter = *null::MainLoop::serverArbiter;
+        serverArbiter.listen("127.0.0.1", 5000);
+        serverArbiter.launch();
+        while (true) {
+            std::string oper;
+            std::cin >> oper;
+            if (oper == "exit") {
+                break;
+            }
         }
     }
 }
