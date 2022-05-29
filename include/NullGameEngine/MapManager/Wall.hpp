cfb8971cbd45b791c240fbf5d317c054a8e1e4f6
Artem <artemtarasov678@gmail.com>
2022-05-29 11:24:27 +0700

    forget files

---
+++ include/NullGameEngine/MapManager/Wall.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include "Script.hpp"
+
+namespace null {
+
+    class Wall: public Script{
+    public:
+        Wall(GameObject& object);
+
+        void start() override;
+
+        void update() override;
+
+        void serialize(google::protobuf::Message& message) const override;
+    };
+
+}
